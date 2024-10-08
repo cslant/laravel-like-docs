9b141e938a2f1d397c6f7bf67c558b3b8e1decf7
Tan Nguyen <tannp27@gmail.com>
2024-10-08 08:44:45 +0700

    feat: add SEO content for package homepages

--- homepage/index.tsx
+++ homepage/index.tsx
@@ -3,6 +3,7 @@ import Heading from "@theme/Heading";
 import Layout from "@theme/Layout";
 
 import "./styles.scss";
+import Head from "@docusaurus/core/lib/client/exports/Head";
 
 const HeaderData = {
   title: "Documentation For Laravel Like Package",
@@ -16,23 +17,35 @@ function HomepageHeader() {
   const { title, description, startButtonLink, startButtonLabel } = HeaderData;
   
   return (
-    <header className="main_header">
-      <div className="container">
-        <div className="row">
-          <div className="col col--5 left_header">
-            <Heading as="h1" className="hero__title main_title">
-              {title}
-            </Heading>
-            <p className="hero__subtitle">{description}</p>
-            <div className="buttons">
-              <Link className="button button--info button--lg" to={startButtonLink}>
-                {startButtonLabel}
-              </Link>
+    <>
+      <Head>
+        <title>Homepage | Laravel Like Package | CSlant Documentation</title>
+        <link rel="canonical" href="https://docs.cslant.com/laravel-like" />
+        <meta name="description"
+              content="Laravel Like package is the interaction for User 👍 like, 👎 dislike, and love ❤️ features for Laravel Application. This package is easy to use and can be integrated into any Laravel application."
+              data-rh="true" />
+        <meta name="keywords"
+              content="Laravel, Like, Dislike, Love, Laravel Like, Laravel Dislike, Laravel Love, Laravel Like Package"
+              data-rh="true" />
+      </Head>
+      <header className="main_header">
+        <div className="container">
+          <div className="row">
+            <div className="col col--5 left_header">
+              <Heading as="h1" className="hero__title main_title">
+                {title}
+              </Heading>
+              <p className="hero__subtitle">{description}</p>
+              <div className="buttons">
+                <Link className="button button--info button--lg" to={startButtonLink}>
+                  {startButtonLabel}
+                </Link>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    </header>
+      </header>
+    </>
   );
 }
 
